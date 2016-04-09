33b4c8963d66317586d4c122ac380e07dc442351
foremaro <foremaro@mail.gvsu.edu>
2016-04-08 22:09:23 -0400

    console app done. additional features will be implemented in a windows form app

--- Mortgage Calculator/Program.cs
+++ Mortgage Calculator/Program.cs
@@ -10,6 +10,52 @@ namespace Mortgage_Calculator
     {
         static void Main(string[] args)
         {
+            // declare variables
+            double principle = 0;
+            double years = 0;
+            double interest = 0;
+            string principleInput, yearsInput, interestInput;
+
+            Console.WriteLine("Enter loan amount in dollars...");
+            principleInput = Console.ReadLine();
+            principle = double.Parse(principleInput);
+            if (principle < 1000)
+            {
+                Console.WriteLine("You got a mortgage for that much? Please enter a more realistic amount");
+                principle = 0;
+            }
+
+            // years
+            Console.WriteLine("Enter the length of the loan in years...");
+            yearsInput = Console.ReadLine();
+            years = double.Parse(yearsInput);
+            if (years < 1)
+            {
+                Console.WriteLine("Please enter a valid length");
+                years = 0;
+            }
+
+            // interest rate
+            Console.WriteLine("Enter your interest rate...");
+            interestInput = Console.ReadLine();
+            interest = double.Parse(interestInput);
+            if (interest < 0)
+            {
+                Console.WriteLine("Dang, how'd you get that rate? Please enter a rate greater than zero...");
+                interest = 0;
+            }
+
+            // calculate the monthly payment
+            double loan = (interest / 1200.0);
+            double numMonths = years * 12;
+            double negNumMonths = 0 - numMonths;
+            double monthlyPymt = principle * loan / (1 - System.Math.Pow((1 + loan), negNumMonths));
+
+            // output
+            Console.WriteLine(string.Format("Monthly payment: {0}{1:0.00}", "$", monthlyPymt));
+            Console.ReadLine();
+
+
         }
     }
 }
